3d1a4323357591ab84ddf4f8f6c2208bcd8782b4
Olivier Trepanier <olitrepanier@hotmail.com>
2018-05-14 12:14:01 -0400

    Removing project files

--- sketchNode/Engine/SerializeTable.lua
+++
@@ -1,9 +0,0 @@
-local HTTP = game:GetService("HttpService")
-
-return function(list)
-	local content = {}
-	for _, element in ipairs(list) do
-		table.insert(content, element.Serialize())
-	end
-	return HTTP:JSONEncode(content)
-end
--- sketchNode/Engine/Settings.lua
+++
@@ -1,5 +0,0 @@
-local module = {}
-
-module.DataFolderName = "SketchEngineGameData"
-
-return module
