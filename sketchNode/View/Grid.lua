c14af35a1b0962bd18c211bff0821b6ad34c9aae
PLVTON <emiletrepanier@hotmail.com>
2018-05-27 21:59:04 -0400

    Added Grid module to manage the background grid

---
+++ sketchNode/View/Grid.lua
@@ -0,0 +1,24 @@
+-- \Description: Module to draw the grid.
+
+local module = {}
+
+function module.Start(parent)
+    --\Doc: Setup the grid.
+    parent = class.Package.Utils.Tests.GetArguments(
+        {'GuiBase', parent}
+    )
+    module.gridImage = module.Package.Utils.Create'ImageButton'{
+        BorderSizePixel = 0,
+        BackgroundColor3 = Color3.fromRGB(49, 49, 49),
+        AnchorPoint = Vector2.new(0.5, 0.5),
+        Name = "Grid",
+        Size = UDim2.new(1, 0, 1, 0),
+        Position = UDim2.new(0.5, 0, 0.5, 0),
+        Image = "rbxassetid://1837504812",
+        ScaleType = Enum.ScaleType.Tile,
+        TileSize = UDim2.new(0, 128, 0, 128),
+        Parent = parent
+    }
+end
+
+return module
--- sketchNode/View/Main.lua
+++ sketchNode/View/Main.lua
@@ -3,9 +3,11 @@ local module = {}
 function module.Start(plugin, Engine)
     -- build the actual UI
     module.gui = plugin:CreateDockWidgetPluginGui("SketchEngineView", module.Package.ViewSettings.DockWidgetInfo)
+    module.gui.Title = "sketchnode"
+    module.gui.Name = "sketchnode"
 
     -- read the current content from the Engine
-
+    module.Package.Grid.Start(module.gui)
     -- connect to events to receive new content
 end
 
