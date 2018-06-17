e20d7732687ffd7e73f4be2b4d78e8cdcf5b649f
Olivier Trepanier <olitrepanier@hotmail.com>
2018-06-17 00:13:43 -0400

    Add the EventNode (FunctionNode now inherit of it)

--- sketchNode/Engine/Classes/FunctionNode.lua
+++ sketchNode/Engine/Classes/FunctionNode.lua
@@ -5,10 +5,8 @@ local class = {
 }
 
 function class.Init()
-	class.__super = {class.Package.Classes.Node}
-	class.__signals = {
-
-	}
+	class.__super = {class.Package.Classes.EventNode}
+	class.__signals = {}
 end
 
 function class.New(o, reference) --\ReturnType: table
@@ -20,10 +18,7 @@ function class.New(o, reference) --\ReturnType: table
 	
 	--class.Package.Utils.Signal.SetSignals(class, o)
 	
-	class.Package.Classes.Node.New(o, x, y)
-	
-	o.referenceClass = reference:GetClassName()
-	o.reference = reference
+	class.Package.Classes.EventNode.New(o, reference)
     
 	return o
 end
@@ -35,39 +30,22 @@ function class.Load(o, data) --\ReturnType: table
     )
     if o == class then o = class.Package.Utils.Inherit(class) end
 	
-	class.Package.Classes.Node.Load(o, data.superClass)
-	--class.Package.Utils.Signal.SetSignals(class, o)
-
-	o.referenceClass = data.referenceClass
-
-	if data.referenceClass == 'LuaReference' then
-		o.reference = class.Package.Classes.LuaReference.Load(o, data.referenceData)
-	elseif data.referenceClass == 'ObjectReference' then
-		o.reference = class.Package.Classes.ObjectReference.Load(o, data.referenceData)
-	else
-		warn(string.format('Data corrupted: Unexpected reference class <%s>', data.referenceClass))
-	end
-
+	class.Package.Classes.EventNode.Load(o, data.superClass)
+    --class.Package.Utils.Signal.SetSignals(class, o)
+    
 	return o
 end
 
 function class:Serialize() --\ReturnType: table
     --\Doc: Serializes all the object data in a table to be reloaded using the Load method.
 	return {
-		referenceClass = self.referenceClass,
-		referenceData = self.reference:Serialize(),
-		superClass = class.Package.Classes.Node.Serialize(self)
+		superClass = class.Package.Classes.EventNode.Serialize(self)
 	}
 end
 
 function class:GetArguments() --\ReturnType: table
-    --\Doc: Returns a list of the arguments of the function
+    --\Doc: Returns a list of the arguments of the function.
     return self.reference:GetArguments()
-end 
-
-function class:GetReturnValues() --\ReturnType: table
-    --\Doc: Returns a list of the arguments of the function
-    return self.reference:GetReturnValues()
 end
 
 return class
